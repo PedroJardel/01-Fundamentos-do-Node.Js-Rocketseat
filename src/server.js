64d020a82890c95507a832b69868db8e3d2bc66a
Pedro Jardel Zago Lima <pedrozagolima@gmail.com>
2024-05-27 23:34:22 -0300

    Diferença entre Stateful e Stateless, metadados(Headers) e JSON (JavaScript Object Notion)

--- src/server.js
+++ src/server.js
@@ -11,13 +11,27 @@ import http from 'node:http';
 // GET /users => Buscando usuários do back-end
 // POST /users => Criar um usuário no back-end
 
+// Stateful - Stateless
+
+// Cabeçalhos (Requisição/Resposta) = Metadados (informações adicionais que não tem haver com o dado retornado, mas sim como esse dado por ser interpretado pelo front e back-end)
+
+//  JSON - JavaScript Object Notation
+
+const users = []
+
 const server = http.createServer((req, res) => {
     const { method, url } = req
 
     if(method === 'GET' && url === '/users') {
-        return res.end('Listagem de usuário')
+        return res.setHeader('Content-type', 'application/json')
+        .end(JSON.stringify(users))
     }
     if(method === 'POST' && url === '/users') {
+        users.push({
+            id: 1,
+            nome: "Jhon Doe",
+            email: "jhondoe@example.com"
+        })
         return res.end('Criação de usuário')
     }
     return res.end('Hello Ignite')
